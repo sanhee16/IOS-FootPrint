2da51fe2e906cb279534e184adbd8a60bf3d2910
sanhee16 <sinhioa20@gmail.com>
2022-11-03 18:47:22 +0900

    init project

---
+++ Footprint/FootprintApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootprintApp.swift
+//  Footprint
+//
+//  Created by Studio-SJ on 2022/11/03.
+//
+
+import SwiftUI
+
+@main
+struct FootprintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
