bf3752167a673f55bcbec114335436d55d2842ec
sanhee16 <sinhioa20@gmail.com>
2023-01-03 18:15:39 +0900

    ui 수정
    - 친구추가하기 버튼 안보이게 수정

--- Footprint/UI/AddFootprint/PeopleWithSelector/PeopleWithSelectorView.swift
+++ Footprint/UI/AddFootprint/PeopleWithSelector/PeopleWithSelectorView.swift
@@ -89,18 +89,20 @@ struct PeopleWithSelectorView: View {
                     .frame(width: geometry.size.width - 24, height: scrollViewHeight, alignment: .leading)
                     .padding([.top, .bottom], 10)
                 }
-                Text("\($vm.serachText.wrappedValue) 추가하기")
-                    .font(.kr16b)
-                    .foregroundColor(.white)
-                    .frame(width: geometry.size.width - 24, height: 50, alignment: .center)
-                    .background(
-                        RoundedRectangle(cornerRadius: 8)
-                            .foregroundColor(!$vm.isMatching.wrappedValue && !$vm.serachText.wrappedValue.isEmpty ? .greenTint1 : .gray30)
-                    )
-                    .onTapGesture {
-                        vm.onClickAddPeople()
-                    }
-                    .padding(.bottom, 10)
+                if !$vm.serachText.wrappedValue.isEmpty && !$vm.isMatching.wrappedValue {
+                    Text("\($vm.serachText.wrappedValue) 추가하기")
+                        .font(.kr16b)
+                        .foregroundColor(.white)
+                        .frame(width: geometry.size.width - 24, height: 50, alignment: .center)
+                        .background(
+                            RoundedRectangle(cornerRadius: 8)
+                                .foregroundColor(!$vm.isMatching.wrappedValue && !$vm.serachText.wrappedValue.isEmpty ? .greenTint1 : .gray30)
+                        )
+                        .onTapGesture {
+                            vm.onClickAddPeople()
+                        }
+                        .padding(.bottom, 10)
+                }
             }
             .padding(EdgeInsets(top: 0, leading: 0, bottom: safeBottom, trailing: 0))
             .ignoresSafeArea(.container, edges: [.top, .bottom])
