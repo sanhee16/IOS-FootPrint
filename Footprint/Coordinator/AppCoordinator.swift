83fd520a96635f5ebe6b6e383798643ac4f2dec9
sanhee16 <sinhioa20@gmail.com>
2023-01-18 18:15:51 +0900

    개념 추가
    - scene delegate 개념 추가

--- Footprint/Coordinator/AppCoordinator.swift
+++ Footprint/Coordinator/AppCoordinator.swift
@@ -12,11 +12,16 @@ class AppCoordinator: Coordinator, Terminatable {
     // UIWindow = 화면에 나타나는 View를 묶고, UI의 배경을 제공하고, 이벤트 처리행동을 제공하는 객체 = View들을 담는 컨테이너
     let window: UIWindow
     
+    /*
+     SceneDelegate에서 window rootViewController 설정해줘야 하는데 window 여기로 가지고와서 여기서 설정해줌
+     */
     init(window: UIWindow) { // SceneDelegate에서 호출
         self.window = window
         super.init() // Coordinator init
         let navigationController = UINavigationController()
         self.navigationController = navigationController // Coordinator의 navigationController
+        
+        // rootViewController 지정 + makeKeyAndVisible 호출 = 지정한 rootViewController가 상호작용을 받는 현재 화면으로 세팅 완료
         self.window.rootViewController = navigationController // window의 rootViewController
         window.makeKeyAndVisible()
     }
