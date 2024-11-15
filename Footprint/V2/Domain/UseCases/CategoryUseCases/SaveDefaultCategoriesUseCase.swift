03a23373853f319cabaa42f5350b8ea13568e9e5
sanhee16 <sinhioa20@gmail.com>
2024-11-15 17:29:18 +0900

    fix: 카테고리 아이템 수정
    - 순서 맞추기
    - 넘버링 추가
    - 이미지 변경

--- Footprint/V2/Domain/UseCases/CategoryUseCases/SaveDefaultCategoriesUseCase.swift
+++ Footprint/V2/Domain/UseCases/CategoryUseCases/SaveDefaultCategoriesUseCase.swift
@@ -17,12 +17,12 @@ class SaveDefaultCategoriesUseCase {
     
     func execute() {
         // basic icons
-        self.categoryRepository.addCategory(nil, name: "기본", color: CategoryColor.black.rawValue, icon: CategoryIcon.emojiFace.rawValue, isDeletable: false)
-        self.categoryRepository.addCategory(nil, name: "데이트", color: CategoryColor.pink.rawValue, icon: CategoryIcon.heart.rawValue, isDeletable: true)
-        self.categoryRepository.addCategory(nil, name: "맛집", color: CategoryColor.orange.rawValue, icon: CategoryIcon.forkSpoon.rawValue, isDeletable: true)
-        self.categoryRepository.addCategory(nil, name: "일상", color: CategoryColor.yellow.rawValue, icon: CategoryIcon.chatSmiley.rawValue, isDeletable: true)
-        self.categoryRepository.addCategory(nil, name: "여행", color: CategoryColor.green.rawValue, icon: CategoryIcon.airport.rawValue, isDeletable: true)
-        self.categoryRepository.addCategory(nil, name: "운동", color: CategoryColor.blue.rawValue, icon: CategoryIcon.groupRunning.rawValue, isDeletable: true)
-        self.categoryRepository.addCategory(nil, name: "취미", color: CategoryColor.purple.rawValue, icon: CategoryIcon.brush.rawValue, isDeletable: true)
+        self.categoryRepository.addCategory(nil, name: "기본", color: CategoryColor.black.rawValue, icon: CategoryIcon.emotion_01_emojiFace.rawValue, isDeletable: false)
+        self.categoryRepository.addCategory(nil, name: "데이트", color: CategoryColor.pink.rawValue, icon: CategoryIcon.emotion_14_heart.rawValue, isDeletable: true)
+        self.categoryRepository.addCategory(nil, name: "맛집", color: CategoryColor.orange.rawValue, icon: CategoryIcon.daily_03_forkSpoon.rawValue, isDeletable: true)
+        self.categoryRepository.addCategory(nil, name: "일상", color: CategoryColor.yellow.rawValue, icon: CategoryIcon.emotion_10_chatSmiley.rawValue, isDeletable: true)
+        self.categoryRepository.addCategory(nil, name: "여행", color: CategoryColor.green.rawValue, icon: CategoryIcon.daily_14_airplane.rawValue, isDeletable: true)
+        self.categoryRepository.addCategory(nil, name: "운동", color: CategoryColor.blue.rawValue, icon: CategoryIcon.activity_07_groupRunning.rawValue, isDeletable: true)
+        self.categoryRepository.addCategory(nil, name: "취미", color: CategoryColor.purple.rawValue, icon: CategoryIcon.activity_01_brush.rawValue, isDeletable: true)
     }
 }
--- Footprint/V2/Presentation/Common/CategoryListEditView/CategoryEditView/CategoryEditVM.swift
+++ Footprint/V2/Presentation/Common/CategoryListEditView/CategoryEditView/CategoryEditVM.swift
@@ -43,19 +43,100 @@ class CategoryEditVM: BaseViewModel {
     ]
     
     let emotionIcons: [CategoryIcon] = [
-        .alien, .chatSmiley, .chatSquare_question, .chatSquare_warning, .emojiCheerful, .emojiDevastated, .emojiExcited, .emojiFace, .emojiHungry, .emojiLol, .emojiSad, .handOk, .heart, .heartBreak, .magicWand, .skull, .star, .thumbsUp, .twinkle
+        .emotion_01_emojiFace,
+        .emotion_02_emojiSad,
+        .emotion_03_emojiCheerful,
+        .emotion_04_emojiLol,
+        .emotion_05_emojiExcited,
+        .emotion_06_emojiHungry,
+        .emotion_07_emojiDevastated,
+        .emotion_08_alien,
+        .emotion_09_skull,
+        .emotion_10_chatSmiley,
+        .emotion_11_handOk,
+        .emotion_12_thumbsUp,
+        .emotion_13_star,
+        .emotion_14_heart,
+        .emotion_15_heartBreak,
+        .emotion_16_chatSquare_warning,
+        .emotion_17_chatSquare_question,
+        .emotion_18_magicWand,
+        .emotion_19_twinkle
     ]
+    
     let dailyIcons: [CategoryIcon] = [
-        .airport, .baggage, .beach, .bicycle, .burger, .bus, .car, .carrot, .chicken, .coffee_mug, .donut, .forkSpoon, .gift, .home, .location_pin, .location_user, .medicalBandage, .money_piggy, .money, .shoppingBasket, .shoppingCart, .smoking, .strawberry, .waterMelon, .wine
+        .daily_01_coffee_mug,
+        .daily_02_wine,
+        .daily_03_forkSpoon,
+        .daily_04_chicken,
+        .daily_05_burger,
+        .daily_06_donut,
+        .daily_07_strawberry,
+        .daily_08_waterMelon,
+        .daily_09_carrot,
+        .daily_10_smoking,
+        .daily_11_bicycle,
+        .daily_12_car,
+        .daily_13_bus,
+        .daily_14_airplane,
+        .daily_15_baggage,
+        .daily_16_beach,
+        .daily_17_medicalBandage,
+        .daily_18_money,
+        .daily_19_money_piggy,
+        .daily_20_shoppingCart,
+        .daily_21_shoppingBasket,
+        .daily_22_gift,
+        .daily_23_home,
+        .daily_24_location_pin,
+        .daily_25_location_user
     ]
+    
     let activityIcons: [CategoryIcon] = [
-        .baseball_batBall, .bowlingSet, .brush, .camera_video, .camera, .controller_wireless, .fitnessBicycle, .golfHole, .groupRunning, .headphones, .mike, .robot, .tambourine, .yoga
+        .activity_01_brush,
+        .activity_02_robot,
+        .activity_03_controller_wireless,
+        .activity_04_camera,
+        .activity_05_mike,
+        .activity_06_camera_video,
+        .activity_07_groupRunning,
+        .activity_08_yoga,
+        .activity_09_fitnessBicycle,
+        .activity_10_golfHole,
+        .activity_11_baseball_batBall,
+        .activity_12_bowlingSet,
+        .activity_13_tambourine,
+        .activity_14_headphones
     ]
+    
     let natureIcons: [CategoryIcon] = [
-        .cat, .cloud, .dog, .fire, .lightning, .petPaw, .quill, .snow, .sun, .tree, .umbrella
+        .nature_01_cat,
+        .nature_02_dog,
+        .nature_03_petPaw,
+        .nature_04_tree,
+        .nature_05_quill,
+        .nature_06_sun,
+        .nature_07_cloud,
+        .nature_08_lightning,
+        .nature_09_umbrella,
+        .nature_10_snow,
+        .nature_11_fire
     ]
+    
     let etcIcons: [CategoryIcon] = [
-        .bell, .bookmark, .calendar_check, .chat_twoBubbles, .cursorArrow1, .cursorArrow2, .letter, .lightBulb, .loading, .lock, .megaPhone, .moustache, .phone
+        .etc_01_bookmark,
+        .etc_02_cursorArrow1,
+        .etc_03_cursorArrow2,
+        .etc_04_bell,
+        .etc_05_megaPhone,
+        .etc_06_loading,
+        .etc_07_phone,
+        .etc_08_twoBubbles,
+        .etc_09_letter,
+        .etc_10_check,
+        .etc_11_lightBulb,
+        .etc_12_moustache,
+        .etc_13_lock
     ]
     
     
@@ -78,7 +159,7 @@ class CategoryEditVM: BaseViewModel {
     func saveCategory(_ onFinish: @escaping () -> ()) {
         if !self.isAvailableToSave { return }
         guard let color = self.color, let icon = self.icon else {
-             return
+            return
         }
         
         if self.isLoading { return }
