272705bd39453493e05233ec846f9e4fa67add2b
sanhee16 <sinhioa20@gmail.com>
2024-11-18 11:47:51 +0900

    feat: font 적용
    - 나눔스퀘어라운드 폰트 적용

--- Footprint/V2/Extension/Font+Extension.swift
+++ Footprint/V2/Extension/Font+Extension.swift
@@ -6,24 +6,42 @@
 //
 
 import Foundation
+import UIKit
 import SwiftUI
 
 extension Font {
-    public static let headline1: Font = .system(size: 21, weight: .regular)
-    public static let headline2: Font = .system(size: 16, weight: .bold)
-    public static let headline3: Font = .system(size: 14, weight: .bold)
-    public static let headline4: Font = .system(size: 12, weight: .regular)
-    public static let body1: Font = .system(size: 16, weight: .regular)
-    public static let body2: Font = .system(size: 15, weight: .regular)
-    public static let body3: Font = .system(size: 14, weight: .regular)
-    public static let callout: Font = .system(size: 15, weight: .regular)
-    public static let subhead: Font = .system(size: 14, weight: .regular)
-    public static let footnote: Font = .system(size: 12, weight: .regular)
-    public static let caption1: Font = .system(size: 11, weight: .regular)
-    public static let caption2: Font = .system(size: 10, weight: .regular)
+    public static let headline1: Font = FPFont.regular(size: 21).font
+    public static let headline2: Font = FPFont.bold(size: 16).font
+    public static let headline3: Font = FPFont.bold(size: 14).font
+    public static let headline4: Font = FPFont.regular(size: 12).font
+    public static let body1: Font = FPFont.regular(size: 16).font
+    public static let body2: Font = FPFont.regular(size: 15).font
+    public static let body3: Font = FPFont.regular(size: 14).font
+    public static let callout: Font = FPFont.regular(size: 15).font
+    public static let subhead: Font = FPFont.regular(size: 14).font
+    public static let footnote: Font = FPFont.regular(size: 12).font
+    public static let caption1: Font = FPFont.regular(size: 11).font
+    public static let caption2: Font = FPFont.regular(size: 10).font
     
     
-    public static let btn1: Font = .system(size: 17, weight: .bold)
-    public static let btn2: Font = .system(size: 16, weight: .bold)
-    public static let btn3: Font = .system(size: 15, weight: .regular)
+    public static let btn1: Font = FPFont.bold(size: 17).font
+    public static let btn2: Font = FPFont.bold(size: 16).font
+    public static let btn3: Font = FPFont.regular(size: 15).font
+}
+
+enum FPFont {
+    case regular(size: CGFloat)
+    case bold(size: CGFloat)
+    case light(size: CGFloat)
+    
+    var font: Font {
+        switch self {
+        case .regular(let size):
+            return .custom("NanumSquareRoundOTFR", size: size)
+        case .bold(let size):
+            return .custom("NanumSquareRoundOTFB", size: size)
+        case .light(let size):
+            return .custom("NanumSquareRoundOTFL", size: size)
+        }
+    }
 }
