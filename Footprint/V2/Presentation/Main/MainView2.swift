0a2f4923c5195fbef19e4c7ba9b65c9ee2d888d4
sanhee16 <sinhioa20@gmail.com>
2024-11-20 20:58:14 +0900

    refactor: mapStatusVM 제거
    - main에서 제거

--- Footprint/V2/Presentation/Main/MainView2.swift
+++ Footprint/V2/Presentation/Main/MainView2.swift
@@ -14,7 +14,6 @@ struct MainView2: View {
     @StateObject private var coordinator: Coordinator = Coordinator()
     @StateObject private var vm: MainVM2 = MainVM2()
     @StateObject private var tabBarService: TabBarService = TabBarService()
-//    @StateObject private var mapStatusVM: MapStatusVM = MapStatusVM()
     
     @State private var selectedIndex: Int = 0
     
@@ -22,17 +21,9 @@ struct MainView2: View {
     var body: some View {
         NavigationStack(path: $coordinator.paths) {
             VStack(alignment: .leading, spacing: 0) {
-                
-//                EditNoteView(
-//                    output: coordinator.editNoteOutput,
-//                    location: Location(latitude: 0.0, longitude: 0.0),
-//                    type: .create
-//                )
-//
                 switch $tabBarService.currentTab.wrappedValue {
                 case .map:
                     MapView2(output: coordinator.mapOutput)
-//                        .environmentObject(mapStatusVM)
                 case .footprints:
                     FootprintListViewV2(output: coordinator.footprintListViewOutput)
                 case .trip:
