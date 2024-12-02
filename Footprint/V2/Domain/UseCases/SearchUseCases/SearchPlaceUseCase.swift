f46b01249ab25d27abd576bd0b04b8e7a959d7d1
sanhee16 <sinhioa20@gmail.com>
2024-12-02 22:22:46 +0900

    fix: geocoding 코드 수정

--- Footprint/V2/Domain/UseCases/SearchUseCases/SearchPlaceUseCase.swift
+++ Footprint/V2/Domain/UseCases/SearchUseCases/SearchPlaceUseCase.swift
@@ -12,11 +12,15 @@ class SearchPlaceUseCase {
     
     func execute(_ text: String, location: Location, sessionToken: GMSAutocompleteSessionToken) async -> [SearchEntity] {
         let filter = GMSAutocompleteFilter()
-        let searchBound: Double = 10.0
+        let searchBound: Double = 4.0
         let northEastBounds = CLLocationCoordinate2DMake(location.latitude + searchBound, location.longitude + searchBound);
         let southWestBounds = CLLocationCoordinate2DMake(location.latitude - searchBound, location.longitude - searchBound);
         filter.locationBias = GMSPlaceRectangularLocationOption(northEastBounds, southWestBounds);
         let placesClient: GMSPlacesClient = GMSPlacesClient()
+        
+        /*
+         SessionToken: https://developers.google.com/maps/documentation/places/ios-sdk/usage-and-billing?hl=ko&_gl=1*1eu33bq*_up*MQ..*_ga*MTA4NDY1NTY5MS4xNzMyNTk3Nzg0*_ga_NRWSTWS78N*MTczMjU5Nzc4NC4xLjEuMTczMjU5Nzc4NS4wLjAuMA..#ac-per-request
+         */
         
         return await withCheckedContinuation { continuation in
             var searchItems: [SearchEntity] = []
